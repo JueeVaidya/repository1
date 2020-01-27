ed718a21f260888c819576108ca358bcb55f122b
juee <git@jueevaidya21@gmail.com>
2020-01-28 00:23:44 +0530

    First commit for file

---
+++ Repo1.java
@@ -0,0 +1,8 @@
+import java.util.*
+Class Repo1
+{
+  public static void main()
+{
+    System.out.println("First File in Repository")
+}
+}
---
+++ n.py
@@ -0,0 +1,5 @@
+a=int(input("enter number")
+for(int i=0;i<a;i++)
+{
+  print(i*"*")
+}
