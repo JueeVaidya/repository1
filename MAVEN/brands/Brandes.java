798ef44fdf4681520494d0e36e156c7d3ab2aa31
Juee Vaidya <60340743+JueeVaidya@users.noreply.github.com>
2020-07-25 12:50:21 +0530

    Add files via upload

---
+++ MAVEN/brands/Brandes.java
@@ -0,0 +1,9 @@
+public abstract class Brandes {
+	protected String brandname ;
+	protected String owner ;
+	protected int price ;
+		
+		public abstract void getBrandOwner() ; 
+	
+
+}
---
+++ MAVEN/brands/Kitkat.java
@@ -0,0 +1,27 @@
+public class Kitkat extends Brandes {
+
+	
+	public Kitkat() {
+		super();
+		this.brandname = "Kit Kat" ;
+		this.owner = "Nestle" ;
+		this.price = 75 ;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+
+	@Override
+	public String toString() {
+		return "--KitKat--\n";
+	}
+
+
+	@Override
+	public void getBrandOwner() {
+		String str = brandname + "is owned by "+owner ;
+		System.out.println(str);
+	}
+
+}
