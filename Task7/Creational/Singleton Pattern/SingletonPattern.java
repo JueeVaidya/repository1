ce1ad64594e7ef5a941608de2dfce8dec07cc6d0
Juee Vaidya <60340743+JueeVaidya@users.noreply.github.com>
2020-07-25 18:50:22 +0530

    Add files via upload

---
+++ Task7/Creational/Singleton Pattern/SingletonPattern.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class SingletonPattern {
+
+public static FileInputStream getFileWrite() throws FileNotFoundException {
+	FileInputStream fin = new FileInputStream("C:\\Users\\Siddharth\\designpatterns.txt") ;
+	return fin ;
+}
+
+public static FileOutputStream getFileRead() throws FileNotFoundException {
+	FileOutputStream fout = new FileOutputStream("C:\\Users\\Siddharth\\designpatterns.txt") ;
+	return fout ;
+}
+	
+}
---
+++ Task7/Structural/Facade/Facaseshapedemo.java
@@ -0,0 +1,9 @@
+public class FacaseShapeDemo {
+	   public static void main(String[] args) {
+		      FacadeShapeMaker shapeMaker = new FacadeShapeMaker();
+
+		      shapeMaker.drawCircle();
+		      shapeMaker.drawRectangle();
+		      shapeMaker.drawSquare();		
+		   }
+}
